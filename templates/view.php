3b0828bebf7525943b383316df0bf895e46f8041
ajh123 <ajh123@users.noreply.github.com>
2022-10-30 18:09:34 +0000

    More execulstions

--- templates/view.php
+++ templates/view.php
@@ -10,15 +10,17 @@
         $path = $root.$name;
 
         if (!($path == $root) && file_exists($path)) {
-            $type = mime_content_type($path);
-            $size = filesize($path);
-            echo("<h1>File `{$name}`</h1>");
-            echo("<p>Type: {$type}. Size: {$size} bytes.<p>");
-            echo("<a href=\"{$name}\">Download here.</a>");
-            if (file_exists($path.".desc")) {
-                $desc = file_get_contents($path.".desc");
-                echo("<br>");
-                echo("<div style=\"margin-top: 40px;\">{$desc}</div>");
+            if (!str_ends_with($name, ".desc") && !str_ends_with($name, ".git") && !str_ends_with($name, "README.md")) {
+                $type = mime_content_type($path);
+                $size = filesize($path);
+                echo("<h1>File `{$name}`</h1>");
+                echo("<p>Type: {$type}. Size: {$size} bytes.<p>");
+                echo("<a href=\"{$name}\">Download here.</a>");
+                if (file_exists($path.".desc")) {
+                    $desc = file_get_contents($path.".desc");
+                    echo("<br>");
+                    echo("<div style=\"margin-top: 40px;\">{$desc}</div>");
+                }
             }
         } else {
             echo("<h1>404 File not found.</h1>");
