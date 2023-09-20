da92f835fdcd2481042a91f9a7e13c57652adf85
ajh123 <ajh123@users.noreply.github.com>
2023-09-20 12:16:20 +0100

    more?

--- apps/frontend/app/routes.js
+++ apps/frontend/app/routes.js
@@ -5,5 +5,22 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const fs = require('fs')
 
 // Add your routes here
+router.get("/:path(*)", function(req, res, next){
+   // Allow the docs.html template to 'include' markdown files
+   var marked = require('marked');
+   var gfmHeadingId = require("marked-gfm-heading-id");
+
+
+   var path = __dirname +"/../../../content/" + req.params.path + ".md";
+   var include = fs.readFileSync(path, 'utf8');
+   marked.marked.use(gfmHeadingId);
+   var html = marked.marked(include, {
+      "gfm": true
+   });
+
+
+   res.render('layouts/post', {"md": html});
+});
