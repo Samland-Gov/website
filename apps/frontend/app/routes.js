bb11e2ea72a3e8db90054871f57ba5b1276cb5eb
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-09-21 12:08:47 +0000

    Fix bug in index page

--- apps/frontend/app/routes.js
+++ apps/frontend/app/routes.js
@@ -13,7 +13,7 @@ const gfmHeadingId = require("marked-gfm-heading-id");
 const renderer = require("./renderer").renderer;
 
 router.get("/:path(*)", function(req, res, next){
-   var path = __dirname +"/../../../content/index.md";
+   var path = __dirname +"/../../../content" + req.params.path + "/index.md";
 
    if (!fs.existsSync(path)) {
       path = __dirname +"/../../../content" + req.params.path + ".md";
