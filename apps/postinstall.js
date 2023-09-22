70dec345159e76ca841d3d2baf57e260f77b92a3
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-09-22 12:41:20 +0000

    Update symlinks

--- apps/postinstall.js
+++ apps/postinstall.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
 const path = require('path')
-var relativePath = path.resolve(__dirname, '../node_modules')
-var relativeTargetPath = path.resolve(__dirname, './frontend/node_modules')
 
-if (!fs.existsSync(relativeTargetPath)) {
-    fs.symlinkSync(relativePath, relativeTargetPath, 'dir')
+function makeSymlink(source, target) {
+    var relativePath = path.resolve(__dirname, source)
+    var relativeTargetPath = path.resolve(__dirname, target)
+
+    if (!fs.existsSync(relativeTargetPath)) {
+        fs.symlinkSync(relativePath, relativeTargetPath, 'dir')
+    }
 }
+
+makeSymlink('../node_modules', './frontend/node_modules')
+makeSymlink('../docs', '../content/government/organisations/department-for-digital/developers/services/website')
