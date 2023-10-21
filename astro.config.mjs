000c1d9326e1b043aa7082296a936f0efe0dcd1b
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-10-21 12:35:51 +0000

    Head fixes and more GOVUK styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import gridsome from '@noxify/gridsome-remark-classes'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://minersonline.uk',
+	site: 'https://minersonline.uk/samland',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [[gridsome, {
+			'heading[depth=1]': 'govuk-heading-xl',
+			'heading[depth=2]': 'govuk-heading-l',
+			'heading[depth=3]': 'govuk-heading-m',
+			'heading[depth=4]': 'govuk-heading-s',
+			// 'heading[depth=5]': 'govuk-caption-s',
+			// 'heading[depth=6]': 'govuk-caption-m',
+			'paragraph': 'govuk-body',
+			'list': 'govuk-body'
+		}]]
+	}
 });
