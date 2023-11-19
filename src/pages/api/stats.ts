e3e5995cfcd71c37afb7900234b14577a30f019c
ajh123 <ajh123@users.noreply.github.com>
2023-11-19 14:08:42 +0000

    test3

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -1,14 +1,14 @@
 export async function GET(context) {
-    // const runtime = context.locals.runtime;
+    const runtime = context.locals.runtime;
 
-    // const { SAMLAND_USERS: storage } = runtime.env as {
-    //     SAMLAND_USERS: KVNamespace;
-    // };
+    const { SAMLAND_USERS: storage } = runtime.env as {
+        SAMLAND_USERS: KVNamespace;
+    };
     // const c = runtime.env;
 
     // let data = await storage.list();
     return new Response(JSON.stringify({
-            context: context,
+            context: storage,
             // storage: storage,
             // keys: data.keys
         }), {
