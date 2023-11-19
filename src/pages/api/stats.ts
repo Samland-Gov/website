66f5c5d7a920b6b851d67baeebf97dfc9e3e5e2d
ajh123 <ajh123@users.noreply.github.com>
2023-11-19 13:47:54 +0000

    test

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -4,9 +4,14 @@ export async function GET(context) {
     const { SAMLAND_USERS: storage } = runtime.env as {
         SAMLAND_USERS: KVNamespace;
     };
+    const c = runtime.env;
 
     let data = await storage.list();
-    return new Response(JSON.stringify(data.keys), {
+    return new Response(JSON.stringify({
+            env: c,
+            storage: storage,
+            keys: data.keys
+        }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json',
