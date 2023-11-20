1977d15a8fabf339d2bf44a087eec66a17018e36
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-11-20 11:09:31 +0000

    Fix error when body is empty

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -19,44 +19,57 @@ export async function POST(context: APIContext) {
     const userRepository = getRepository(storage);
 
     try {
-        const { email, password } = await context.request.json() as RequestData;
-        if (email != undefined && password != undefined ) {
-            const userExist = await userRepository.getUserBy({id: email, matchField: 'email'});
-            if (!userExist) {
+        const body = (await context.request.text())
+        
+        if (body.length > 0) {
+            const { email, password } = JSON.parse(body) as RequestData;
+            if (email != undefined && password != undefined ) {
+                const userExist = await userRepository.getUserBy({id: email, matchField: 'email'});
+                if (!userExist) {
+                    return new Response(JSON.stringify({
+                            error: "User does not exist! Please register your account before logging in!",
+                            status: 409
+                        }),{
+                            status: 409,
+                            headers: {'Content-Type': 'application/json'}
+                        }
+                    );
+                }
+
+                // Compare user passwords
+                const passwordCorrect = userExist && (await bcrypt.compare(password, userExist.data.password));
+                if (passwordCorrect) {
+                    const authToken = await jwt.sign(userExist.data, AUTH_TOKEN_KEY!);
+
+                    return new Response(JSON.stringify({
+                            user: {
+                                user_id: userExist.id,
+                                full_name: userExist.data.full_name,
+                                email: userExist.data.email,
+                                auth_token: authToken,
+                            },
+                            status: 200
+                        }),{
+                            status: 200,
+                            headers: {'Content-Type': 'application/json'}
+                        }
+                    );
+                } else {
+                    return new Response(JSON.stringify({
+                            error: "Invalid credentials!",
+                            status: 400
+                        }),{
+                            status: 400,
+                            headers: {'Content-Type': 'application/json'}
+                        }
+                    );
+                }
+            } else {
                 return new Response(JSON.stringify({
-                        error: "User does not exist! Please register your account before logging in!",
+                        error: "Invalid parameters. You must supply, `email`, and `password`!",
                         status: 409
                     }),{
                         status: 409,
-                        headers: {'Content-Type': 'application/json'}
-                    }
-                );
-            }
-
-            // Compare user passwords
-            const passwordCorrect = userExist && (await bcrypt.compare(password, userExist.data.password));
-            if (passwordCorrect) {
-                const authToken = await jwt.sign(userExist.data, AUTH_TOKEN_KEY!);
-
-                return new Response(JSON.stringify({
-                        user: {
-                            user_id: userExist.id,
-                            full_name: userExist.data.full_name,
-                            email: userExist.data.email,
-                            auth_token: authToken,
-                        },
-                        status: 200
-                    }),{
-                        status: 200,
-                        headers: {'Content-Type': 'application/json'}
-                    }
-                );
-            } else {
-                return new Response(JSON.stringify({
-                        error: "Invalid credentials!",
-                        status: 400
-                    }),{
-                        status: 400,
                         headers: {'Content-Type': 'application/json'}
                     }
                 );
