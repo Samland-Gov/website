18b4fb435691471192b79487f2bfed89ae573031
ajh123 <ajh123@users.noreply.github.com>
2024-03-02 14:35:13 +0000

    docs: support 2024 legislation

--- post-install.js
+++ post-install.js
@@ -1,15 +1,23 @@
 import { symlink } from 'fs/promises';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
+import fs from "fs";
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Define the source and target folders for the symbolic link
-const sourceFolder = __dirname+'/legislation/2023';
-const targetFolder = __dirname +'/src/content/legislation/2023';
+async function buildLegislationDirectory(name) {
+    // Define the source and target folders for the symbolic link
+    const sourceFolder = __dirname+'/legislation/'+name;
+    const targetFolder = __dirname +'/src/content/legislation/'+name;
 
-// Create a symbolic link
-const symlinkPath = join(targetFolder);
-await symlink(sourceFolder, symlinkPath, 'dir');
+    // Create a symbolic link
+    const symlinkPath = join(targetFolder);
+    if (!fs.existsSync(symlinkPath)) {
+        await symlink(sourceFolder, symlinkPath, 'dir');
+    }
+}
+
+buildLegislationDirectory("2023")
+buildLegislationDirectory("2024")
